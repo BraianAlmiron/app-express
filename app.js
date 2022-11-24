446767596d03d80f0ab1d2cbfd0ac02d9a1a9fb6
BraianAlmiron <braianpatricio1@gmail.com>
2022-11-24 11:02:05 -0300

    AGREGA VIEWS

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3030;
+const path = require('path');
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','registro.html')));
+app.get('/detail',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname,'views','carrito.html')));
+app.get('*',(req,res) => res.send('NO SE ENCUENTRA LA RUTA :('));
 
 
 app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}`))
